96c6cdc9ed909bc2b5f5b5f0fad0b86ce3706664
Mark Tran <markmtran1@gmail.com>
2021-02-03 12:13:28 -0500

    trying firestore

--- src/components/history/History.js
+++ src/components/history/History.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import { Button } from '@material-ui/core';
+import firebase from 'firebase/app';
+
+var db = firebase.firestore();
 
 const History = () => {
+  const handleAdd = () => {
+    db.collection("users").add({
+      first: "Ada",
+      last: "Lovelace",
+      born: 1815
+    })
+    .then(function(docRef) {
+        console.log("Document written with ID: ", docRef.id);
+    })
+    .catch(function(error) {
+        console.error("Error adding document: ", error);
+    });
+  }
   return (
-    <div></div>
+    <Button onClick={handleAdd}></Button>
   );
 }
 
